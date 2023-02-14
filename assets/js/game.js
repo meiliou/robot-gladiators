fc00ed7a51a9772c27785ced2c0c7c3986315c62
meiliou <83768277+meiliou@users.noreply.github.com>
2023-02-14 11:08:11 +0200

    loop to play all robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,32 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
-for (var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
-
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// Game States
-// "WIN" - Player robot has defeated all enemy robots
-//    * Fight all enemy robots
-//    * Defeat each enemy robot
-// "LOSE" - Player robot's health is zero or less
+/* Game States
+ "WIN" - Player robot has defeated all enemy robots
+    * Fight all enemy robots
+    * Defeat each enemy robot
+ "LOSE" - Player robot's health is zero or less*/
 
-
-var fight = function() {
+var fight = function(enemyName) {
     // Alert user that they are starting the round  
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to 'FIGHT' or 'SKIP' this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -39,20 +24,20 @@ var fight = function() {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-        playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-        window.alert(enemyNames + " has died!");
+        window.alert(enemyName + " has died!");
     } else {
-        window.alert(enemyNames + " still has " + enemyHealth + " health left.");
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-        enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -83,4 +68,6 @@ var fight = function() {
 
 };
 
-// fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
