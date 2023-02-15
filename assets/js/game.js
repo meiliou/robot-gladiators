c40e4f303dcd575b57ebc2dc9f02cf2795d514ee
meiliou <83768277+meiliou@users.noreply.github.com>
2023-02-15 22:15:57 +0200

    add shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,7 +12,7 @@ var playerMoney = 10;
 // You can also log multiple values at onnce like this:
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyNames = ["Roberto", "Android", "Blackberry", "iOS"];
+var enemyNames = ["Roberto", "Amy Android", "R2D2"];
 var enemyAttack = 10;
 var enemyHealth = 50;
 
@@ -107,6 +107,12 @@ var startGame = function() {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+
+            /*//if we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+              shop();
+            }*/
+
         } else {
             window.alert("You have lost your robot player in battle! Game Over!");
             break;
@@ -133,6 +139,14 @@ var endGame = function() {
       window.alert("Thank you for playing Robot Gladiators! Come back soon!")
   }
 };
+
+/*
+var shop = function() {
+  console.log("entered the shop");
+};
+*/
+
+
 
 startGame();
 
